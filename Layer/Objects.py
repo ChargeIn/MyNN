eae7d02ea5d42813fe513db3af14b37c211a0c0d
Florian Plesker <florian.plesker@web.de>
2019-06-17 19:15:21 +0200

    base nn works working on conv2d

--- Layer/Objects.py
+++ Layer/Objects.py
@@ -1,7 +1,7 @@
 # Florian Plesker 2026588
 
 import numpy as np
-from PIL import Image
+import scipy.signal as sc
 
 
 # Describing the shape of an tensor
@@ -14,9 +14,21 @@ class Shape:
 # Representation of the data
 class Tensor:
 
-    def __init__(self, shape, elements):
-        self.elements = elements
-        self.shape = shape
+    def __init__(self, elements):
+        self.elements = np.array(elements)
+        self.deltas = {}
+
+    def get_deltas(self):
+        if "deltas" not in self.deltas:
+            return np.zeros(self.elements.shape)
+        return self.deltas["deltas"]
+
+    def add_deltas(self, update):
+        self.deltas["deltas"] = self.get_deltas() + update
+
+    def update(self, rate, batchsize):
+        self.elements = self.elements - rate * self.get_deltas()/batchsize
+        self.deltas = {}
 
 
 # Converts pictures to tensors
@@ -24,54 +36,83 @@ class InputLayer:
 
     @staticmethod
     def forward(img):
-        img = img.reshape((-1, 1))
+        img = img.reshape((1, -1))
         return img
 
 
 # Representation of Layers
+class Conv2DLayer:
+
+    def __init__(self, filters, count):
+        self.filters = filters
+        self.count = count
+
+    def forward(self, inputs):
+        out = np.zeros([int(inputs.shape[0] - np.floor(self.filters.shape[1]/2) + 1), int(inputs.shape[1]
+                        - np.floor(self.filters.shape[2]/2) + 1, self.count)])
+        for i in range(0, self.count):
+            for j in range(0, self.filters.shape[3]):
+                out[i, :, :] += sc.convolve2d(inputs, self.filters[i, j, :, :])
+
+        return out
+
+    #def backprop(self, lastlayer):
+
+
 
 class FullyConnected:
 
     def __init__(self, shape_in, shape_out, bias):
-        self.inputs = np.zeros(shape_in)
-        self.weights = np.random.rand(shape_out, shape_in[0])
-        self.bias = bias
+        self.inputs = Tensor(np.zeros(shape_in))
+        self.weights = Tensor(np.random.rand(shape_in[1], shape_out))
+        self.bias = Tensor(bias)
 
     def get_weights(self):
         return self.weights
 
     def forward(self, inputs):
-        self.inputs = inputs
-        return np.matmul(self.weights, inputs) + self.bias
+        self.inputs.elements = inputs
+        return np.matmul(inputs, self.weights.elements) + self.bias.elements
 
-    def backward(self, lastlayer):
-        self.deltas = lastlayer
-        return np.matmul(lastlayer, np.transpose(self.weights))
+    def backprop(self, lastlayer):
+        self.bias.add_deltas(lastlayer)
+        self.weights.add_deltas(np.matmul(np.transpose(self.inputs.elements), lastlayer))
+        return np.matmul(lastlayer, np.transpose(self.weights.elements))
+
+    def update(self, rate, batchsize):
+        self.bias.update(rate, batchsize)
+        self.weights.update(rate, batchsize)
 
 
 class ReLuLayer:
 
     def __init__(self, shape):
-        self.inputs = np.zeros(shape)
+        self.inputs = Tensor(np.zeros(shape))
 
     def forward(self, inputs):
-        self.inputs = inputs
+        self.inputs.elements = inputs
         return np.maximum(inputs, 0)
 
-    def backward(self, lastlayer):
-        return np.matmul(np.diag(np.where(self.inputs > 0, 1, 0)), lastlayer)
+    def backprop(self, lastlayer):
+        return np.matmul(lastlayer, np.diag(np.where(self.inputs.elements > 0, 1, 0).flatten()))
+
+    def update(self, rate, batchsize):
+        return
 
 
 class Softmax:
 
     def __init__(self, shape):
-        self.inputs = np.zeros(shape)
+        self.inputs = Tensor(np.zeros(shape))
 
     def forward(self, inputs):
-        self.inputs = inputs
-        exp = np.exp(inputs)
-        return exp/np.sum(exp)
+        exp = np.exp(inputs - np.max(inputs, axis=1))
+        self.inputs.elements = exp/np.sum(exp, axis=1)
+        return self.inputs.elements
 
-    def backward(self, lastlayer):
-        return np.matmul(lastlayer, np.diagflat(self.inputs) - np.matmul(self.inputs, np.transpose(self.inputs)))
+    def backprop(self, lastlayer):
+        s = self.inputs.elements.reshape(-1, 1)
+        return np.matmul(lastlayer, np.diagflat(s) - np.dot(s, s.T))
 
+    def update(self, rate, btachsize):
+        return
--- Layer/__init__.py
+++ Layer/__init__.py
@@ -1,10 +1,15 @@
 # Florian Plesker 2026588
 from Layer.Objects import *
 import tensorflow as tf
+import numpy as np
 
 
 def meanSquare(output, original):
-    return np.linalg.norm(output - original)/original.size
+    return np.linalg.norm(output - original)
+
+
+def meanSquareDerivation(output, original):
+    return output - original
 
 
 def crossEntropy(output, original):
@@ -12,30 +17,67 @@ def crossEntropy(output, original):
     return np.multiply(log, original)
 
 
+def intToVector(i, shape):
+    vector = np.zeros([1, shape])
+    vector[0, i] = 1
+    return vector
+
+
 if __name__ == '__main__':
 
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, y_train = x_train / 255.0, y_train / 255.0
+    x_train, x_test = x_train / 255.0, x_test / 255.0
 
     inputLayer = InputLayer()
-    fully = FullyConnected([784, 1], 10, 1)
+    fully = FullyConnected([1, 784], 10, 1)
+    filter1 = np.array([[[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]])
+    filter2 = np.array([[[1, 0, -1], [0, 0, 0], [-1, 0, 1]]])
+    filters = np.array([filter1, filter2])
+    convLayer = Conv2DLayer(filters, 2)
     reLu = ReLuLayer([10, 1])
     soft = Softmax([10, 1])
 
+    # starting parameter
+    batchsize = 50
+    learning_rate = 0.1
     layers = [fully, reLu, soft]
+    epoche = 1
+    output = 0
+    loss = 0
+    old_loss = 999
     while True:
-        for i in range(y_train.size):
-            x1 = x_train[i]
-            x2 = y_train[i]
-            print(x1.shape)
+        for i in range(0, y_train.size, batchsize):
+            loss = 0
+            for j in range(0, batchsize):
+                x1 = x_train[i + j]
+                x2 = intToVector(y_train[i + j], 10)
+                x1 = convLayer.forward(x1)
+                inputs = inputLayer.forward(x1)
+                for l in layers:
+                    inputs = l.forward(inputs)
+
+                output = meanSquareDerivation(inputs, x2)
+                for l in layers[::-1]:
+                    output = l.backprop(output)
+
+            for l in layers:
+                l.update(learning_rate, batchsize)
+
+            print("\rEpoche: " + str(epoche) + " Training: " + str(i) + " / " + str(y_train.size), end="")
+
+        print(" \rEpoche: " + str(epoche) + " Training: " + str(y_train.size) + " / " + str(y_train.size))
+
+        for i in range(0, y_test.size, batchsize):
+            x1 = x_test[i]
+            x2 = intToVector(y_test[i], 10)
             inputs = inputLayer.forward(x1)
-            print(inputs.shape)
             for l in layers:
                 inputs = l.forward(inputs)
-            output = meanSquare(inputs, x2)
-            print(output)
-            for l in layers[::-1]:
-                output = l.backward(output)
-                #l.update()
-            print(output)
+            loss += meanSquare(inputs, x2)
+
+        if np.abs(loss - old_loss) < 0.001:
+            break
+        old_loss = loss
+        print("Epoche: " + str(epoche) + " Loss:" + str(loss / batchsize))
+        epoche += 1
