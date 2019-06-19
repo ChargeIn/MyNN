c4d0e0268a46708cda9e36aa7797d57da58a7e24
Florian Plesker <florian.plesker@web.de>
2019-06-19 14:49:51 +0200

    added sigmoid works fine on fully test txt

--- Layer/Objects.py
+++ Layer/Objects.py
@@ -1,7 +1,8 @@
 # Florian Plesker 2026588
 
 import numpy as np
-from PIL import Image
+import scipy.signal as sc
+import scipy.special as sp
 
 
 # Describing the shape of an tensor
@@ -69,10 +70,10 @@ class reshapeLyer:
 
 class FullyConnected:
 
-    def __init__(self, shape_in, shape_out, bias):
+    def __init__(self, shape_in, shape_out):
         self.inputs = Tensor(np.zeros(shape_in))
         self.weights = Tensor(np.random.rand(shape_in[1], shape_out))
-        self.bias = Tensor(bias)
+        self.bias = Tensor(np.ones(shape_out))
 
     def get_weights(self):
         return self.weights
@@ -88,40 +89,97 @@ class FullyConnected:
 
     def update(self, rate, batchsize):
         self.bias.update(rate, batchsize)
-        test = self.weights.elements
         self.weights.update(rate, batchsize)
-        #print((self.weights.elements - test).min())
+
+
+# Representation of Layers
+class Conv2DLayer:
+
+    def __init__(self, filters, bias):
+        self.filters = Tensor(filters)
+        self.bias = Tensor(bias)
+        self.inputs = {}
+
+    def forward(self, inputs):
+        self.inputs["in"] = inputs
+        d1 = inputs.shape[1] - self.filters.elements.shape[2] + 1
+        d2 = inputs.shape[2] - self.filters.elements.shape[3] + 1
+        out = np.zeros([self.filters.elements.shape[0], d1, d2])
+        for i in range(0, self.filters.elements.shape[0]):
+            for j in range(0, self.filters.elements.shape[1]):
+                out[i, :, :] += sc.convolve2d(inputs[j, :, :], self.filters.elements[i, j, :, :], mode="valid") \
+                                + self.bias.elements[i, j, :]
+        return np.array([out])
+
+    def backprop(self, lastlayer):
+        newfilter = np.transpose(self.filters.elements, [1, 0, 2, 3])
+        out = np.zeros(self.inputs["in"].shape)
+        for i in range(0, newfilter.shape[0]):
+            for j in range(0, newfilter.shape[1]):
+                newfilter[i, j, :, :] = np.rot90(newfilter[i, j, :, :], 2)
+                out += sc.convolve2d(lastlayer[i, j, :, :], newfilter[i, j, :, :], mode="valid")
+
+        # generating updates for deltas
+        filter_update = np.zeros(self.filters.elements.shape)
+        bias_update = np.zeros(self.bias.elements.shape)
+        for i in range(0, newfilter.shape[0]):
+            for j in range(0, newfilter.shape[1]):
+                filter_update[i, j, :, :] = sc.convolve2d(self.inputs["in"][j, :, :], lastlayer[i, j, :, :], mode="valid")
+                bias_update[i, j, :] = np.array(np.sum(lastlayer[j, :, :]))
+        self.filters.add_deltas(filter_update)
+        self.bias.add_deltas(bias_update)
+        return out
+
+    def update(self, rate, batchsize):
+        self.filters.update(rate, batchsize)
+        self.bias.update(rate, batchsize)
 
 
 class ReLuLayer:
 
-    def __init__(self, shape):
-        self.inputs = Tensor(np.zeros(shape))
+    def __init__(self):
+        self.inputs = {}
 
     def forward(self, inputs):
-        self.inputs.elements = inputs
+        self.inputs["in"] = Tensor(inputs)
         return np.maximum(inputs, 0)
 
     def backprop(self, lastlayer):
-        return np.matmul(lastlayer, np.diag(np.where(self.inputs.elements > 0, 1, 0).flatten()))
+        return np.matmul(lastlayer, np.diag(np.where(self.inputs["in"].elements > 0, 1, 0).flatten()))
 
     def update(self, rate, batchsize):
         return
 
 
+class Sigmoid:
+
+    def __init__(self):
+        self.inputs = {}
+
+    def forward(self, inputs):
+        self.inputs["in"] = sp.expit(inputs)
+        return self.inputs["in"]
+
+    def backprop(self, lastlayer):
+        return self.inputs["in"]*(1 - self.inputs["in"])*lastlayer
+
+    def update(self):
+        return
+
+
 class Softmax:
 
-    def __init__(self, shape):
-        self.inputs = Tensor(np.zeros(shape))
+    def __init__(self):
+        self.inputs = {}
 
     def forward(self, inputs):
         exp = np.exp(inputs - np.max(inputs, axis=1))
-        self.inputs.elements = exp/np.sum(exp, axis=1)
-        return self.inputs.elements
+        self.inputs["in"] = Tensor(exp/np.sum(exp, axis=1))
+        return self.inputs["in"].elements
 
     def backprop(self, lastlayer):
-        s = self.inputs.elements.reshape(-1, 1)
+        s = self.inputs["in"].elements.reshape(-1, 1)
         return np.matmul(lastlayer, np.diagflat(s) - np.dot(s, s.T))
 
-    def update(self, rate, btachsize):
+    def update(self, rate, batchsize):
         return
--- Layer/__init__.py
+++ Layer/__init__.py
@@ -4,41 +4,33 @@ import tensorflow as tf
 import numpy as np
 
 
-def meanSquare(output, original):
-    return np.linalg.norm(output - original)
+def mean_square(output, original):
+    return np.linalg.norm(original - output)
 
 
-def meanSquareDerivation(output, original):
+def mean_square_derivation(output, original):
     return output - original
 
 
-def crossEntropy(output, original):
+def cross_entropy(output, original):
     log = - np.log(output)
     return np.multiply(log, original)
 
-def intToVector(i,shape):
+
+def int_to_vector(i, shape):
     vector = np.zeros([1, shape])
     vector[0, i] = 1
     return vector
 
 
-if __name__ == '__main__':
-
+def minst(layers):
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    input = InputLayer()
-    reshape = reshapeLyer()
-    fully = FullyConnected([1, 784], 10, 1)
-    fully2 = FullyConnected([1, 10], 10, 1)
-    reLu = ReLuLayer([10, 1])
-    soft = Softmax([10, 1])
-
     # starting parameter
     batchsize = 50
     learning_rate = 0.1
-    layers = [input, reshape,fully, reLu, soft]
     epoche = 1
     output = 0
     loss = 0
@@ -46,18 +38,15 @@ if __name__ == '__main__':
         for i in range(0, y_train.size, batchsize):
             loss = 0
             for j in range(0, batchsize):
-                input = x_train[i+j]
-                x2 = intToVector(y_train[i+j], 10)
+                inputs = x_train[i + j]
+                x2 = int_to_vector(y_train[i + j], 10)
 
                 for l in layers:
-                    input = l.forward(input)
+                    inputs = l.forward(inputs)
 
-                output = meanSquareDerivation(input, x2)
+                output = mean_square_derivation(inputs, x2)
                 for l in layers[::-1]:
                     output = l.backprop(output)
-                    if(i+j == 10):
-                        loss = 0
-                        #print(output)
 
             for l in layers:
                 l.update(learning_rate, batchsize)
@@ -66,10 +55,68 @@ if __name__ == '__main__':
 
         print(" \rEpoche: " + str(epoche) + " Training: " + str(y_train.size) + " / " + str(y_train.size))
         for i in range(0, y_test.size, batchsize):
-            input = x_test[i]
-            x2 = intToVector(y_test[i], 10)
+            inputs = x_test[i]
+            x2 = int_to_vector(y_test[i], 10)
             for l in layers:
-                input = l.forward(input)
-            loss += meanSquare(input, x2)
-        print("Epoche: " + str(epoche) + " Loss:" + str(loss/batchsize))
+                inputs = l.forward(inputs)
+            loss += mean_square(inputs, x2)
+        print("Epoche: " + str(epoche) + " Loss:" + str(loss / batchsize))
         epoche += 1
+
+
+if __name__ == '__main__':
+
+    inputs = InputLayer()
+    reshape = reshapeLyer()
+    filter1 = np.array([[[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]])
+    filter2 = np.array([[[1, 0, -1], [0, 0, 0], [-1, 0, 1]]])
+    filters = np.array([filter1, filter2])
+    bias = np.array([[[1]], [[1]]])
+    convLayer = Conv2DLayer(filters, bias)
+    fully = FullyConnected([1, 1352], 10)#784
+    fully2 = FullyConnected([1, 100], 10)
+    reLu1 = ReLuLayer()
+    reLu = ReLuLayer()
+    soft = Softmax()
+
+    layers = [inputs, convLayer, reshape, fully, reLu, soft]
+    # layers = [inputs, reshape, fully, reLu1, fully2, reLu, soft]
+    #minst(layers)
+
+    #fully connected test
+    inputTest = InputLayer()
+    reshapeTest = reshapeLyer()
+    fullyTest = FullyConnected([1, 3], 2)
+    fullyTest.bias.elements = np.array([0.0000, 0.0000, 0.0000])
+    fullyTest.weights.elements = np.array([[-0.5057, 0.3987, -0.8943], [0.3356, 0.1673, 0.8321], [-0.3485, -0.4597, -0.1121]])
+    fullyTest2 = FullyConnected([1, 3], 2)
+    fullyTest2.weights.elements = np.array([[0.4047, 0.9563], [-0.8192, -0.1274], [0.3662, -0.7252]])
+    fullyTest2.bias.elements = np.array([[0.0000, 0.0000]])
+    sigmoidTest = Sigmoid()
+    softmaxTest = Softmax()
+
+    # forward
+    input = [0.4183, 0.5209, 0.0291]
+    label = [0.7095, 0.0942]
+    input = inputTest.forward(input)
+    input = reshapeTest.forward(input)
+    input = fullyTest.forward(input)
+    print("forward fully1: ", input)
+    input = sigmoidTest.forward(input)
+    print("forward sigmoid: ", input)
+    input = fullyTest2.forward(input)
+    print("forward fully 2: ", input)
+    input = softmaxTest.forward(input)
+    print("forward softmax: ", input)
+
+    # backward
+    back = np.array([[-1.4901, -0.1798]])
+    back = softmaxTest.backprop(back)
+    print("backward softmax: ", back)
+    back = fullyTest2.backprop(back)
+    print("backward fully2: ", back)
+    back = sigmoidTest.backprop(back)
+    print("backward sigmoid: ", back)
+    back = fullyTest.backprop(back)
+    print("backward fully1: ", back)
+
